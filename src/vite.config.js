45018196c7354419828818e7ab622b3f8c02aadb
amcgrathsantowski <amcgrathsantowski@gmail.com>
2025-02-21 18:54:02 -0500

    move vite config to src

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
-import pwaConfig from './pwa.config';
+import pwaConfig from '../pwa.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
