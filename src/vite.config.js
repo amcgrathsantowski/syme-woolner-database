a62265263ea72a9ae7236deaefc89ef2ccfe28f6
amcgrathsantowski <amcgrathsantowski@gmail.com>
2025-02-22 21:24:13 -0500

    update build outdir

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 import pwaConfig from '../pwa.config';
+import {fileURLToPath} from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA(pwaConfig)],
   build: {
-    outDir: './public/dist',
+    outDir: `${__dirname}/public/dist`,
     emptyOutDir: true
   },
-  base: '/public/dist/',
   server: {
     port: 3000
   }
